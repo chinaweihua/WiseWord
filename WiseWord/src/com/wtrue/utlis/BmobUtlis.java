afb72bb8e90f61e0641a9467053f222130dd3dba
Ben <caoxinbo@163.com>
2017-01-06 10:48:26 +0800

    增加退出登陆功能、获取收藏列表功能和测试代码

--- WiseWord/src/com/wtrue/utlis/BmobUtlis.java
+++ WiseWord/src/com/wtrue/utlis/BmobUtlis.java
@@ -1,8 +1,10 @@
 package com.wtrue.utlis;
 
+
+import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.datatype.BmobFile;
-import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 import cn.bmob.v3.listener.UpdateListener;
 
@@ -78,7 +80,7 @@ public class BmobUtlis {
 	 * @param content	内容
 	 * @param image		图片
 	 */
-	public static void userColllection(String title,String content,BmobFile image,SaveListener<String> listener){
+	public static void userCollection(String title,String content,BmobFile image,SaveListener<String> listener){
 		WWUser user = BmobUser.getCurrentUser(WWUser.class);
 		Collection collection = new Collection();
 		collection.setTitle(title);
@@ -92,7 +94,7 @@ public class BmobUtlis {
 	 * @param title		标题
 	 * @param content	内容
 	 */
-	public static void userColllection(String title,String content,SaveListener<String> listener){
+	public static void userCollection(String title,String content,SaveListener<String> listener){
 		WWUser user = BmobUser.getCurrentUser(WWUser.class);
 		Collection collection = new Collection();
 		collection.setTitle(title);
@@ -106,12 +108,23 @@ public class BmobUtlis {
 	 * @param title		标题
 	 * @param image		图片
 	 */
-	public static void userColllection(String title,BmobFile image,SaveListener<String> listener){
+	public static void userCollection(String title,BmobFile image,SaveListener<String> listener){
 		WWUser user = BmobUser.getCurrentUser(WWUser.class);
 		Collection collection = new Collection();
 		collection.setTitle(title);
 		collection.setImage(image);
 		collection.setAuthor(user);
 		collection.save(listener);
+	}
+	/**
+	 * 获取当前用户的收藏列表（如果没有登陆报错）
+	 * @param findListener	请求回调
+	 */
+	public static void getUserCollection(FindListener<Collection> findListener){
+		WWUser user = BmobUser.getCurrentUser(WWUser.class);
+		BmobQuery<Collection> bmobQuery = new BmobQuery<Collection>();
+		bmobQuery.addWhereEqualTo("author", user.getObjectId());
+		bmobQuery.setLimit(10);
+		bmobQuery.findObjects(findListener);
 	}
 }
--- WiseWord/src/com/wtrue/wiseword/MainActivity.java
+++ WiseWord/src/com/wtrue/wiseword/MainActivity.java
@@ -1,11 +1,16 @@
 package com.wtrue.wiseword;
 
+import java.util.List;
+
 import okhttp3.Call;
 
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 import cn.bmob.v3.listener.UpdateListener;
 
+import com.wtrue.bean.Collection;
+import com.wtrue.interfaces.BmobExtendListener;
 import com.wtrue.netmonitor.NetUtils.NetType;
 import com.wtrue.okhttp.Callback;
 import com.wtrue.okhttp.OkHttpUtils;
@@ -30,14 +35,15 @@ import android.widget.Toast;
  * @author Ben
  * 
  */
-public class MainActivity extends BaseActivity implements OnClickListener{
+public class MainActivity extends BaseActivity implements OnClickListener {
 	private TextView main_tv;
 	private boolean isConn;// 是否有网络
 	private MyStringCallback callback;
 	/**
-	 * 注册、登陆、修改密码、更新名字、收藏测试按钮
+	 * 注册、登陆、修改密码、更新名字、收藏测试、获取收藏列表、退出登陆按钮
 	 */
-	private Button register_bt,login_bt,reset_bt,update_bt,colletion;
+	private Button register_bt, login_bt, reset_bt, update_bt, colletion,
+			query,logout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +55,8 @@ public class MainActivity extends BaseActivity implements OnClickListener{
 		reset_bt = (Button) this.findViewById(R.id.reset_bt);
 		update_bt = (Button) this.findViewById(R.id.update_bt);
 		colletion = (Button) this.findViewById(R.id.colletion);
+		query = (Button) this.findViewById(R.id.query);
+		logout = (Button) this.findViewById(R.id.logout);
 		callback = new MyStringCallback();
 		OkHttpUtils.get().url("http://route.showapi.com/341-3")
 				.addParams("showapi_appid", "100")
@@ -61,6 +69,8 @@ public class MainActivity extends BaseActivity implements OnClickListener{
 		reset_bt.setOnClickListener(this);
 		update_bt.setOnClickListener(this);
 		colletion.setOnClickListener(this);
+		query.setOnClickListener(this);
+		logout.setOnClickListener(this);
 	}
 
 	@Override
@@ -101,7 +111,7 @@ public class MainActivity extends BaseActivity implements OnClickListener{
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
-		OkHttpUtils.getInstance().cancelTag(this);//当activity退出的时候取消请求
+		OkHttpUtils.getInstance().cancelTag(this);// 当activity退出的时候取消请求
 	}
 
 	@Override
@@ -109,44 +119,81 @@ public class MainActivity extends BaseActivity implements OnClickListener{
 		// TODO Auto-generated method stub
 		Intent intent;
 		switch (arg0.getId()) {
-		case R.id.register_bt://跳转注册界面
-			intent = new Intent(MainActivity.this,RegisterActivity.class);
+		case R.id.register_bt:// 跳转注册界面
+			intent = new Intent(MainActivity.this, RegisterActivity.class);
 			startActivity(intent);
 			break;
-		case R.id.login_bt://跳转登陆界面
-			intent = new Intent(MainActivity.this,LoginActivity.class);
+		case R.id.login_bt:// 跳转登陆界面
+			intent = new Intent(MainActivity.this, LoginActivity.class);
 			startActivity(intent);
 			break;
-		case R.id.reset_bt://修改密码
-			intent = new Intent(MainActivity.this,ResetActivity.class);
+		case R.id.reset_bt:// 修改密码
+			intent = new Intent(MainActivity.this, ResetActivity.class);
 			startActivity(intent);
 			break;
-		case R.id.update_bt://更新名字
+		case R.id.update_bt:// 更新名字
 			BmobUtlis.userUpdate(null, "哈哈", new UpdateListener() {
-				
+
 				@Override
 				public void done(BmobException arg0) {
 					// TODO Auto-generated method stub
-					if(arg0 == null){//更新成功
-						Toast.makeText(MainActivity.this, "更新成功", Toast.LENGTH_SHORT).show();
-					}else{
-						Toast.makeText(MainActivity.this, "更新失败了+："+arg0.toString(), Toast.LENGTH_SHORT).show();
+					if (arg0 == null) {// 更新成功
+						Toast.makeText(MainActivity.this, "更新成功",
+								Toast.LENGTH_SHORT).show();
+					} else {
+						Toast.makeText(MainActivity.this,
+								"更新失败了+：" + arg0.toString(), Toast.LENGTH_SHORT)
+								.show();
 					}
 				}
 			});
 			break;
-		case R.id.colletion://收藏测试   收藏之前需要判断是否有登陆过，如果没有登陆或者本地保存的账号密码过期需要提示用户重新登陆
-			BmobUtlis.userColllection("测试", "测试aaaa", new SaveListener<String>() {
-				 
+		case R.id.colletion:// 收藏测试 收藏之前需要判断是否有登陆过，如果没有登陆或者本地保存的账号密码过期需要提示用户重新登陆
+			BmobUtlis.userCollection("测试", "测试aaaa",
+					new SaveListener<String>() {
+
+						@Override
+						public void done(String arg0, BmobException arg1) {
+							// TODO Auto-generated method stub
+							if (arg1 == null) {// 收藏成功
+								Toast.makeText(MainActivity.this, "收藏成功了",
+										Toast.LENGTH_SHORT).show();
+							} else {// 收藏失败
+								Toast.makeText(MainActivity.this, "收藏失败了",
+										Toast.LENGTH_SHORT).show();
+								Log.e("失败原因", arg1.toString());
+							}
+						}
+					});
+			break;
+		case R.id.query:// 获取收藏列表 如果当前没有登陆的话会报错
+			BmobUtlis.getUserCollection(new FindListener<Collection>() {
 				@Override
-				public void done(String arg0, BmobException arg1) {
+				public void done(List<Collection> arg0, BmobException arg1) {
 					// TODO Auto-generated method stub
-					if(arg1 == null){//收藏成功
-						Toast.makeText(MainActivity.this, "收藏成功了", Toast.LENGTH_SHORT).show();
-					}else{//收藏失败
-						Toast.makeText(MainActivity.this, "收藏失败了", Toast.LENGTH_SHORT).show();
-						Log.e("失败原因", arg1.toString());
+					if (arg1 == null) {
+						for (Collection gameScore : arg0) {
+							Log.d("获取的收藏列表：", gameScore.getTitle());
+						}
+					} else {
+						Log.e("失败", arg1.toString());
 					}
+				}
+			});
+			break;
+		case R.id.logout://退出登陆
+			BmobUtlis.userLogOut(new BmobExtendListener() {
+				
+				@Override
+				public void logOut() {
+					// TODO Auto-generated method stub
+					Log.d("退出登陆", true+"");
+				}
+				
+				@Override
+				public void errorLogOut() {
+					// TODO Auto-generated method stub
+					Log.e("退出登陆", false+"");
 				}
 			});
 			break;
